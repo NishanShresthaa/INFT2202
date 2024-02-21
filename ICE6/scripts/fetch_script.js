430178e2032e919673ed3dfc49b779d8d5cb99c0
NishanShresthaa <shresthanishan793@gmail.com>
2024-02-21 09:49:30 -0500

    Added and updated the fetch_script file as shown in the class

---
+++ ICE6/scripts/fetch_script.js
@@ -0,0 +1,38 @@
+console.log("fetch_script.js loaded");
+
+// get the button for fetch
+let btn_fetch = $('#xhrJoke');
+
+// create a url variable
+let url_fetch = "https://icanhazdadjoke.com/";
+
+//need a header Accept value set to 'application/json'
+let myFetchHeader = {"Accept": "application/json"};
+
+// create the callback for the click
+$(btn_fetch).click(() => {
+
+
+    // use fetch
+    fetch(url_fetch, {
+
+        // give the necessary header data
+        headers: myFetchHeader
+
+    // first then() to receive the promise
+    }).then((result) => {
+        // send the json from the promise on to the next then()
+        return result.json();
+
+    })
+    .then((jsonRes) => {
+            
+            // console log the json
+            console.log(jsonRes);
+
+            // TODO : set the output
+            document.getElementById('output').innerText = jsonRes.joke;
+
+        })
+
+});
